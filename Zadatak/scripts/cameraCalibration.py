3eb7cf280ac5b62d3750a3133709c91346b8513f
DavidHorvat <davidhorvatdvd@gmail.com>
2024-02-17 14:55:54 +0100

    Camera calibration

---
+++ Zadatak/scripts/cameraCalibration.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('camera_cal\*.jpg')
+mean_error = 0
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+        
+        # Perform calibration here
+        ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera([objp], [corners2], gray.shape[::-1], None, None)
+        imgpoints2, _ = cv.projectPoints(objp, rvecs[0], tvecs[0], mtx, dist)
+        error = cv.norm(corners2, imgpoints2, cv.NORM_L2)/len(imgpoints2)
+        mean_error += error
+        print("Mean error for image {}: {}".format(fname, mean_error))
+        print( "total error: {}".format(mean_error/len(objpoints)) )
+        np.savez('output/cameraCalibration/calibration.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+        
+cv.destroyAllWindows()
+    
