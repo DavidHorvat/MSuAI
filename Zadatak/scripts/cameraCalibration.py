4c5712eb05aea157253a93a4683478903981994b
DavidHorvat <davidhorvatdvd@gmail.com>
2024-02-17 15:57:10 +0100

    Starting undistortion

--- Zadatak/scripts/cameraCalibration.py
+++ Zadatak/scripts/cameraCalibration.py
@@ -5,22 +5,26 @@ import glob
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+#Setting the necessary variables
+numRows = 6 
+numColumns = 9 
+mean_error = 0
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((numRows*numColumns,3), np.float32)
+objp[:,:2] = np.mgrid[0:numRows,0:numColumns].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 images = glob.glob('camera_cal/*.jpg')
-mean_error = 0
 
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (numRows,numColumns), None)
     
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -29,20 +33,20 @@ for fname in images:
         imgpoints.append(corners2)
         
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)   
-    cv.imshow('img', img)
-    cv.waitKey(500)
+        cv.drawChessboardCorners(img, (numRows,numColumns), corners2, ret)   
+        cv.imshow('img', img)
+        cv.waitKey(500)
     
-    # Perform calibration here
+# Perform calibration here
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-# for i in range(len(objpoints)):
-#     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-#     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-#     mean_error += error
+
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
     
-# print("Mean error for image {}: {}".format(fname, mean_error))
-# print( "total error: {}".format(mean_error/len(objpoints)) )
+print( "total error: {}".format(mean_error/len(objpoints)) )
+
 np.savez('output/cameraCalibration/calibration.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
-        
-# cv.destroyAllWindows()
+cv.destroyAllWindows()
     
